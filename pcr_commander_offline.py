5b42af9883945252920dfa1ff2a276e56df03a87
Stefan Krämer <stefan.kraemer.91@googlemail.com>
2018-10-02 13:37:02 +0200

    add offline version

--- pcr_commander_offline.py
+++ pcr_commander_offline.py
@@ -10,39 +10,6 @@ t_72 = int(input("Duration of 72 degree step [s]:  "))
 number_cycles = int(input("Number of cycles:  "))
 
 
-start_pcr_audio = "audio/start_pcr.mp3"
-start_pcr = gTTS(text="Die PCR startet in 30 sec mit dem 95 grad celcius Schritt. Mach dich bereit!", lang="de")
-start_pcr.save(start_pcr_audio)
-
-start_now_audio = "audio/start_now.mp3"
-start_now = gTTS(text="Start", lang="de")
-start_now.save(start_now_audio)
-
-change_audio = "audio/change.mp3"
-change = gTTS(text="5 Sekunden", lang="de")
-change.save(change_audio)
-
-now_change_audio = "audio/change_now.mp3"
-now_change = gTTS(text="Jetzt wechseln", lang="de")
-now_change.save(now_change_audio)
-
-temp1_audio = "audio/temp1.mp3"
-temp1 = gTTS(text="95 Grad Celcius fuer " + str(t_95) + " sec", lang="de")
-temp1.save(temp1_audio)
-
-temp2_audio = "audio/temp2.mp3"
-temp2 = gTTS(text="60 Grad Celcius fuer " + str(t_60) + " sec", lang="de")
-temp2.save(temp2_audio)
-
-temp3_audio = "audio/temp3.mp3"
-temp3 = gTTS(text="72 Grad Celcius fuer " + str(t_72) + " sec", lang="de")
-temp3.save(temp3_audio)
-
-for i in range(1,number_cycles + 1):
-    step_audio = "audio/step" + str(i) + ".mp3"
-    step = gTTS(text="Zyclus " + str(i), lang="de")
-    step.save(step_audio)
-
 # Save chosen program:
 prog = open("programs/" + experiment_name + "_pcr_prot.txt","w")
 prog.write("Number of cycles:" + "\t" + str(number_cycles) + "\n")
@@ -53,17 +20,25 @@ prog.write("3" + "\t" + "72" + "\t" + str(t_72) + "\n")
 
 prog.close()
 
+# Set audio paths
+start_pcr_audio = "audio_offline/start_pcr.mp3"
+change_audio = "audio_offline/change.mp3"
+start_now_audio = "audio_offline/start_now.mp3"
+temp1_audio = "audio_offline/temp1.mp3"
+temp2_audio = "audio_offline/temp2.mp3"
+temp3_audio = "audio_offline/temp3.mp3"
+
 
 mixer.music.load(start_pcr_audio)
 mixer.music.play()
 while mixer.music.get_busy() == 1:
     continue
-time.sleep(25)
+time.sleep(15)
 mixer.music.load(change_audio)
 mixer.music.play()
 while mixer.music.get_busy() == 1:
     continue
-time.sleep(5)
+time.sleep(3)
 mixer.music.load(start_now_audio)
 mixer.music.play()
 while mixer.music.get_busy() == 1:
@@ -71,59 +46,44 @@ while mixer.music.get_busy() == 1:
 
 print("START")
 for i in range(0,number_cycles):
-    step = "audio/step" + str(i+1) + ".mp3"
     print("##########")
     print("Step:  " + str(i+1))
     print("##########")
     print("95 grad Celcius")
-    mixer.music.load(step)
-    mixer.music.play()
-    while mixer.music.get_busy() == 1:
-        continue
+
+
     mixer.music.load(temp1_audio)
     mixer.music.play()
     while mixer.music.get_busy() == 1:
         continue
-    time.sleep(t_95-4-2-5)
+    time.sleep(t_95-7)
     mixer.music.load(change_audio)
     mixer.music.play()
     while mixer.music.get_busy() == 1:
         continue
     time.sleep(5)
-    mixer.music.load(now_change_audio)
-    mixer.music.play()
-    while mixer.music.get_busy() == 1:
-        continue
 
     print("60 grad celcius")
     mixer.music.load(temp2_audio)
     mixer.music.play()
     while mixer.music.get_busy() == 1:
         continue
-    time.sleep(t_60-4-2-5)
+    time.sleep(t_60-7)
     mixer.music.load(change_audio)
     mixer.music.play()
     while mixer.music.get_busy() == 1:
         continue
     time.sleep(5)
-    mixer.music.load(now_change_audio)
-    mixer.music.play()
-    while mixer.music.get_busy() == 1:
-        continue
     
     print("72 grad celcius")
     mixer.music.load(temp3_audio)
     mixer.music.play()
     while mixer.music.get_busy() == 1:
         continue
-    time.sleep(t_72-4-2-5)
+    time.sleep(t_72-7)
     mixer.music.load(change_audio)
     mixer.music.play()
     while mixer.music.get_busy() == 1:
         continue
     time.sleep(5)
-    mixer.music.load(now_change_audio)
-    mixer.music.play()
-    while mixer.music.get_busy() == 1:
-        continue
 
